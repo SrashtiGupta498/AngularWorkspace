183d6005b0da37487ba64a1139357d86d767fbb1
srashtiGupta <srashtig1009@gmail.com>
2023-02-28 20:26:50 +0530

    001

--- sampletest/src/app/app.module.ts
+++ sampletest/src/app/app.module.ts
@@ -3,14 +3,10 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { ProductComponent } from './product/product.component';
-import { FormsComponent } from './forms/forms.component';
 
 @NgModule({
   declarations: [
-    AppComponent,
-    ProductComponent,
-    FormsComponent
+    AppComponent
   ],
   imports: [
     BrowserModule,
